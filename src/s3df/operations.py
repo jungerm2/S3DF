960e781fa1e48cd108cd6348013ea688a472b64a
Sacha Jungerman <jungerm2@illinois.edu>
2022-05-03 17:04:09 -0500

    add rotations

--- src/s3df/operations.py
+++ src/s3df/operations.py
@@ -1,5 +1,8 @@
 import textwrap
 
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
 from .primitives import Shape
 from .snippets import INDENT
 from .utils import to_vec
@@ -48,7 +51,7 @@ class Translate(Operation):
         return f"{self.__class__.__name__}(direction={self.direction}, shape={self.shapes[0]})"
 
     def __repr__(self):
-        self.shapes[0].modify(f"%(p)s - {to_vec(self.direction)}")
+        self.modify(f"%(p)s - {to_vec(self.direction)}")
         # self.shapes[0].modify(f"invert({t})*%(p)s")
         return repr(self.shapes[0])
 
@@ -65,8 +68,47 @@ class Repeat(Operation):
         return f"{self.__class__.__name__}(direction={self.direction}, shape={self.shapes[0]})"
 
     def __repr__(self):
-        self.shapes[0].modify(f"{self.GLSL_NAME}(%(p)s, {to_vec(self.direction)})")
+        self.modify(f"{self.GLSL_NAME}(%(p)s, {to_vec(self.direction)})")
         return repr(self.shapes[0])
+
+
+class Rotate(Operation):
+    def __init__(self, matrix, *shapes):
+        self.matrix = matrix
+        super().__init__(*shapes)
+        self.modify = self.shapes[0].modify
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(matrix={self.matrix}, shape={self.shapes[0]})"
+
+    def __repr__(self):
+        row_major = "mat3(" + ",\n".join(to_vec(row, size=3) for row in self.matrix) + ")"
+        column_major = f"transpose({row_major})"
+        self.modify(f"inverse({column_major})*(%(p)s)")
+        return repr(self.shapes[0])
+
+
+class RotateVec(Rotate):
+    def __init__(self, vec, angle, *shapes):
+        vec = np.array(vec) / np.linalg.norm(vec)
+        angle = np.deg2rad(angle)
+        matrix = R.from_rotvec(angle * vec).as_matrix().tolist()
+        super().__init__(matrix, *shapes)
+
+
+class RotateX(RotateVec):
+    def __init__(self, angle, *shapes):
+        super().__init__([1, 0, 0], angle, *shapes)
+
+
+class RotateY(RotateVec):
+    def __init__(self, angle, *shapes):
+        super().__init__([0, 1, 0], angle, *shapes)
+
+
+class RotateZ(RotateVec):
+    def __init__(self, angle, *shapes):
+        super().__init__([0, 0, 1], angle, *shapes)
 
 
 class Tx(Operation):
